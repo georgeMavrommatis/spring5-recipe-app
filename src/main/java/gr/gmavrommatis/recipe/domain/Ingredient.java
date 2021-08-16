faa2d8a63bad5311d9291a3355919af2c89306c4
George Mavrommatis <gmavrommatis@cognity.gr>
2021-08-16 14:48:04 +0300

    136 done

---
+++ src/main/java/gr/gmavrommatis/recipe/domain/Ingredient.java
@@ -0,0 +1,66 @@
+package gr.gmavrommatis.recipe.domain;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+/**
+ * Ingredient class created 16-Aug-21
+ *
+ * @author G.MAVROMMATIS
+ */
+@Entity
+public class Ingredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    private BigDecimal amount;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure uom;
+
+    @ManyToOne
+    private Recipe recipe;
+
+    public Long getId() {
+        return id;
+    }
+
+    public UnitOfMeasure getUom() {
+        return uom;
+    }
+
+    public void setUom(UnitOfMeasure uom) {
+        this.uom = uom;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+}
