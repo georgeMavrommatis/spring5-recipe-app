1e593b5eff9f5aa2b55407647a7f2b9b31c8567a
George Mavrommatis <gmavrommatis@cognity.gr>
2021-08-23 20:25:37 +0300

    194. Display a Recipe by ID

--- src/test/java/gr/gmavrommatis/recipe/services/RecipeServiceImplTest.java
+++ src/test/java/gr/gmavrommatis/recipe/services/RecipeServiceImplTest.java
@@ -11,6 +11,7 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 import static org.junit.Assert.*;
@@ -45,6 +46,21 @@ public class RecipeServiceImplTest {
         Set<Recipe> recipes = recipeService.getRecipes();
 
         assertEquals(recipes.size(), 1);
-        verify(recipeRepository, times(2)).findAll();
+        verify(recipeRepository, times(1)).findAll();
+    }
+
+    @Test
+    public void getRecipeByIdTest() throws Exception {
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
+
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
+        Recipe recipeReturned = recipeService.findById(1L);
+
+        assertNotNull("Null recipe returned", recipeReturned);
+        verify(recipeRepository, times(1)).findById(anyLong());
+        verify(recipeRepository, never()).findAll();
     }
 }
