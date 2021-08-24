1c44ca4ffe5b7acc8f53b8371797c182e5e39ce0
George Mavrommatis <gmavrommatis@cognity.gr>
2021-08-24 11:21:41 +0300

    199. Create a Recipe

--- src/main/java/gr/gmavrommatis/recipe/controllers/RecipeController.java
+++ src/main/java/gr/gmavrommatis/recipe/controllers/RecipeController.java
@@ -1,10 +1,10 @@
 package gr.gmavrommatis.recipe.controllers;
 
+import gr.gmavrommatis.recipe.commands.RecipeCommand;
 import gr.gmavrommatis.recipe.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * RecipeController class created 23-Aug-21
@@ -24,5 +24,20 @@ public class RecipeController {
     public String showById(@PathVariable String id, Model model){
         model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
         return "recipe/show";
+    }
+
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand recipeCommand){
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(recipeCommand);
+
+        return "redirect:/recipe/show/"+savedCommand.getId();
     }
 }
