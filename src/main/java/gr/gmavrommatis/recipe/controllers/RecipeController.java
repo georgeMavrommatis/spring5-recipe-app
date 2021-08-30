185a093754c0b0a693bbe2038df31bd16b69c093
George Mavrommatis <gmavrommatis@cognity.gr>
2021-08-30 13:43:06 +0300

    229. Showing Error Data on 404 Error Page

--- src/main/java/gr/gmavrommatis/recipe/controllers/RecipeController.java
+++ src/main/java/gr/gmavrommatis/recipe/controllers/RecipeController.java
@@ -1,11 +1,14 @@
 package gr.gmavrommatis.recipe.controllers;
 
 import gr.gmavrommatis.recipe.commands.RecipeCommand;
+import gr.gmavrommatis.recipe.exceptions.NotFoundException;
 import gr.gmavrommatis.recipe.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * RecipeController class created 23-Aug-21
@@ -56,5 +59,21 @@ public class RecipeController {
 
         recipeService.deleteById(Long.valueOf(id));
         return "redirect:/";
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound(Exception exception){
+
+        log.error("Handling not found exception");
+        log.error(exception.getMessage());
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("404error");
+        modelAndView.addObject("exception", exception);
+
+
+        return modelAndView;
     }
 }
